ea005afa9b3a78245690f5e53c1f1dfb12c01ab9
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2023-02-18 12:12:37 -0700

    fixed broken images

--- components/footer.js
+++ components/footer.js
@@ -23,7 +23,7 @@ class Footer extends HTMLElement {
   
       <!-- Copyright -->
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        <span class="me-3">Created by enRocket Dev <img src="/assets/Nina Rocket Icon Black.png" style="height:30px;"></span>
+        <span class="me-3">Created by enRocket Dev <img src="../../assets/Nina Rocket Icon Black.png" style="height:30px;"></span>
       </div>
       <!-- Copyright -->
     </footer>
