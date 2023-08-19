79ede9c8bfab414ca802666075bcf5b6015a6ff4
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2023-08-19 08:41:06 -0700

    file paths and css

--- components/header.js
+++ components/header.js
@@ -44,9 +44,7 @@ class Header extends HTMLElement {
               </li>
             </ul>
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">About</a>
-          </li>
+ 
         </ul>
        
       </div>
